1d3c04527e8971d51eeccecdfd34d2b3accf76a1
Mohak Akul Prakash <mohakprakash2016@gmail.com>
2025-04-16 14:41:05 -0700

    lab3-step3-commit0??

--- global.js
+++ global.js
@@ -1,16 +1,37 @@
-console.log('IT’S ALIVE!');
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  
+  : "/portfolio-106/";         
 
-function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'contact/', title: 'Contact Me' },
+  { url: 'https://github.com/TC960', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+  
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+  
+  nav.append(a);
 }
-
-const navLinks = $$("nav a");
-
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
-
-  currentLink?.classList.add('current');
-
-
-console.log('All be working');
